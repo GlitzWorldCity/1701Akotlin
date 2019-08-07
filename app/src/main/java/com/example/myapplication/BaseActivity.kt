9a3cde7434f201a5dccd93e7b35dcf78a0bcfc84
zhaozhicheng <2547560164@qq.com>
2019-08-07 10:49:19 +0800

    登录界面View的实现

--- app/src/main/java/com/example/myapplication/BaseActivity.kt
+++ app/src/main/java/com/example/myapplication/BaseActivity.kt
@@ -1,11 +1,14 @@
 package com.example.myapplication
 
+import android.app.ProgressDialog
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
 
 abstract class BaseActivity:AppCompatActivity(){
-
+    val progressDiaLog by lazy {
+        ProgressDialog(this)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(getLayoutResId())
@@ -17,4 +20,12 @@ abstract class BaseActivity:AppCompatActivity(){
 
 
     abstract fun getLayoutResId(): Int
+
+    fun showProgress(message:String){
+        progressDiaLog.setMessage(message)
+        progressDiaLog.show()
+    }
+    fun dissmissProgress(){
+        progressDiaLog.dismiss()
+    }
 }
--- app/src/main/java/com/example/myapplication/LoginActivity.kt
+++ app/src/main/java/com/example/myapplication/LoginActivity.kt
@@ -1,12 +1,44 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import android.widget.Toast.LENGTH_SHORT
+import com.example.myapplication.contract.LoginContract
+import kotlinx.android.synthetic.main.activity_login.*
 
-class LoginActivity : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+class LoginActivity :BaseActivity(),LoginContract.View {
+    override fun onUserNameError() {
+        userName.setError(getString(R.string.user_name_error))
     }
+
+    override fun onPasswordError() {
+        password.setError(getString(R.string.password_error))
+    }
+
+    override fun onStartLoging() {
+        //弹出进度条
+        showProgress(getString(R.string.logging))
+    }
+
+    override fun onLoggedInSuccess() {
+        //隐藏进度条
+        dissmissProgress()
+        //进入住界面
+        var intent = Intent(this@LoginActivity, MainActivity::class.java)
+        startActivity(intent)
+        //LoginAotivity
+        finish()
+    }
+
+    override fun onLoggedInFailed() {
+        //隐藏进度条
+        dissmissProgress()
+        //弹出toast
+        Toast.makeText(this,R.string.login_failed,LENGTH_SHORT).show()
+    }
+
+    override fun getLayoutResId(): Int  = R.layout.activity_login
+
 }
