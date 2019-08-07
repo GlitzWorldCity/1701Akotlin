3a9f42dc7b1d3822616a31f26e08ba8df25d1971
zhaozhicheng <2547560164@qq.com>
2019-08-07 11:25:58 +0800

    登录界面实现

--- app/src/main/java/com/example/myapplication/contract/BasePresenter.kt
+++ app/src/main/java/com/example/myapplication/contract/BasePresenter.kt
@@ -1,6 +1,18 @@
 package com.example.myapplication
 
+import android.os.Handler
+import android.os.Looper
+
 interface BasePresenter{
 
+    companion object{
+        val  handler by lazy {
+            Handler(Looper.getMainLooper())
+        }
+    }
+
+    fun uiThread(f: () -> Unit){
+        handler.post { f() }
+    }
 
 }
--- app/src/main/java/com/example/myapplication/preasenter/LoginPresenter.kt
+++ app/src/main/java/com/example/myapplication/preasenter/LoginPresenter.kt
@@ -3,6 +3,9 @@ package com.example.myapplication.preasenter
 import com.example.myapplication.contract.LoginContract
 import com.example.myapplication.extentions.isValiUserName
 import com.example.myapplication.extentions.isValipassword
+import com.hyphenate.chat.EMClient
+import com.example.myapplication.adapter.EMCallBackAdapter
+
 
 class LoginPresenter(val view: LoginContract.View) :LoginContract.Presenter{
 
@@ -17,6 +20,18 @@ class LoginPresenter(val view: LoginContract.View) :LoginContract.Presenter{
         }else view.onUserNameError()
     }
     private fun LoginEaseMob(userName: String, passWord: String) {
+        EMClient.getInstance().login(userName, passWord, object : EMCallBackAdapter() {
 
+            override fun onSuccess() {
+                EMClient.getInstance().groupManager().loadAllGroups();
+                EMClient.getInstance().chatManager().loadAllConversations();
+                //在主线程通知view层
+                uiThread { view.onLoggedInSuccess() }
+            }
+
+            override fun onError(p0: Int, p1: String?) {
+                uiThread { view.onLoggedInFailed() }
+            }
+        })
     }
 }
