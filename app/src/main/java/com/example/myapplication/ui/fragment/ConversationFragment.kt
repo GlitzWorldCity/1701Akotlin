9c9ac28c287b4c0c19ce66abbeb9e4eab851c865
zhaozhicheng <2547560164@qq.com>
2019-08-09 21:14:22 +0800

    会话界面展示完成

--- app/src/main/java/com/example/myapplication/ui/fragment/ConversationFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/fragment/ConversationFragment.kt
@@ -3,11 +3,26 @@ package com.example.myapplication.ui.fragment
 import android.support.v7.widget.LinearLayoutManager
 import com.example.myapplication.R
 import com.example.myapplication.adapter.ConversationListAdapter
+import com.example.myapplication.adapter.EMMesssageListenerAdapter
+import com.hyphenate.chat.EMClient
+import com.hyphenate.chat.EMConversation
+import com.hyphenate.chat.EMMessage
 import kotlinx.android.synthetic.main.fragment_conversation.*
 import kotlinx.android.synthetic.main.header.*
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
 
 class ConversationFragment :BaseFragment(){
+
+    val conversations = mutableListOf<EMConversation>()
+
     override fun getLayoutBaseId(): Int = R.layout.fragment_conversation
+
+    val messageListener = object :EMMesssageListenerAdapter(){
+        override fun onMessageReceived(p0: MutableList<EMMessage>?) {
+            loadConversations()
+        }
+    }
 
     override fun init() {
         super.init()
@@ -15,8 +30,25 @@ class ConversationFragment :BaseFragment(){
         recyclerView.apply {
             setHasFixedSize(true)
             layoutManager = LinearLayoutManager(context)
-            adapter = ConversationListAdapter(context)
+            adapter = ConversationListAdapter(context,conversations)
         }
+        EMClient.getInstance().chatManager().addMessageListener(messageListener)
+        loadConversations()
+    }
+
+    private fun loadConversations() {
+        doAsync {
+            conversations.clear()
+            val  allConversations =  EMClient.getInstance().chatManager().allConversations
+            conversations.addAll(allConversations.values)
+            uiThread { recyclerView.adapter?.notifyDataSetChanged() }
+        }
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        EMClient.getInstance().chatManager().removeMessageListener(messageListener)
     }
 
 }
